c9d91d9361c672d6d110d0a885cadc371a144197
Mariama Diallo <diallomt193@yahoo.com>
2022-04-01 14:18:03 -0400

    I determined the average change, greatest increase and greatest decrease

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,19 +1,13 @@
 #PyBank Challenge
 
 #Import Modules
-
 import os
 import csv
 
 #Define Variables
-months = []
-profitlosschange =[]
-
-count_months = 0
-net_profit_loss = 0
-previous_month_profit_loss = 0
-current_month_profit_loss = 0
-profit_loss_change = 0
+month_count = []
+profit = []
+change_profit = []
 
 #Set path for cvs file
 #csvpath = os.path.join("..", "Resources", "budget_data.csv")
@@ -21,21 +15,32 @@ csvpath = os.path.join("Pybank/Resources/budget_data.csv")
 
 #Open and read csv file
 with open(csvpath) as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=",")
-    # print(csv_reader)
-#     #Read Header Row
-    csv_header=next(csvfile)
-    # print(csv_header)
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+ 
 
-#     #Start looping through the file
-
-    for row in csv_reader:
+    #Start looping through the file
+    for row in csvreader:
     #count the months
-        count_months += 1
+        month_count.append(row[0])
+        profit.append(int(row[1]))
+    for x in range(len(profit)-1):
+        change_profit.append(profit[x+1]-profit[x])
+
+#Evaluate the max and min from the list made
+increase = max(change_profit)
+decrease = min(change_profit)
+
+#Use the index
+month_increase = change_profit.index(max(change_profit))+1
+month_decrease = change_profit.index(min(change_profit))+1
+
+
+
     
     #Net total amount of "Profit/Losses" over the entire period
-        current_month_profit_loss = int(row[1])
-        net_profit_loss += current_month_profit_loss
+        # current_month_profit_loss = int(row[1])
+        # net_profit_loss += current_month_profit_loss
         # print(net_profit_loss)
         # arr= []
         # if (count_months ==1):
@@ -69,10 +74,10 @@ with open(csvpath) as csvfile:
 
     # #Print anylysis
 
-    print("Financial Analysis")
-    print("------------------------")
-    print(f"Total Months: {count_months}")
-    print(f"Total: ${net_profit_loss}")
-    # print(f"Average Change: ${average_profit_loss}")
-    # print(f"Greatest Increase in profits: {best_month} (${highest_change})")
-    # print(f"Greatest Decrease in Losses: {worst_month} (${lowest_change})")
+print("Financial Analysis")
+print("------------------------")
+print(f"Total Months: {len(month_count)}")
+print(f"Total: ${sum(profit)}")
+print(f"Average Change: {round(sum(change_profit)/len(change_profit),2)}")
+print(f"Greatest Increase in profits: {month_count[month_increase]} (${(str(increase))})")
+print(f"Greatest Decrease in profits: {month_count[month_decrease]} (${(str(decrease))})")
