07c4d729e03e22c44ca5f18b4822bee0f256f366
Mariama Diallo <diallomt193@yahoo.com>
2022-04-02 17:56:11 -0400

    determined the election results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,75 +1,95 @@
-#PyBank Challenge
+#PyPoll Challenge
 
 #Import Modules
-
 import os
 import csv
 
-#Define Variables
-months = []
-profitlosschange =[]
+#Set Variable for Pypoll data
+# voters_candidates = []
+# votes_per_candidate = []
+vote_total = 0
+candidate = ""
+final_list = {}
+winner_votes = 0
+winner_name = ""
 
-count_months = 0
-net_profit_loss = 0
-previous_month_profit_loss = 0
-current_month_profit_loss = 0
-profit_loss_change = 0
+#Set path for csv file
+election_data_csv_path = os.path.join("Resources/election_data.csv")
 
-#Set path for cvs file
-#csvpath = os.path.join("..", "Resources", "budget_data.csv")
-csvpath = os.path.join("Homeworks/03-Python/Instructions/PyBank/Resources/budget_data.csv")
+#Open and read csv
 
-#Open and read csv file
-with open(csvpath) as csvfile:
+with open(election_data_csv_path, newline="") as csvfile:
+
     csv_reader = csv.reader(csvfile, delimiter=",")
 
-    #Read Header Row
-    csv_header=next(csvfile)
+    #Read the header row
+    csv_header = next(csvfile)
+    # print(csv_header)
 
-    #Start looping through the file
+    #Loop through data
+    for row in csv_reader:
+        vote_total +=1
 
-for row in csv_reader:
-    #count the months
-    count_months += 1
-    
-    #Net total amount of "Profit/Losses" over the entire period
-    current_month_profit_loss = int(row[1])
-    net_profit_loss += current_month_profit_loss
+    #update candidates and their vote
+        candidate = row[2]
+        if candidate in final_list:
+            final_list[candidate] += 1
+        else:
+            final_list[candidate] = 1
+    #Printint initial results
+    print(f"election results")
+    print(f"-------------")
+    print(f"total votes: {vote_total}")
 
-    if (count_months ==1):
-        previous_month_profit_loss = current_month_profit_loss
+    for candidate in final_list:
 
-    else:
-        profit_loss_change = current_month_profit_loss - previous_month_profit_loss
+        #determine the percentage of candidate vote
+        percentage = (final_list[candidate]/vote_total)*100
+        percentage_formatted = "{:.3f}".format(percentage)
+        print(candidate + ":" + str(percentage_formatted) + "%" + "(" + str(final_list[candidate])+")")
+        #Calculate the number of votes
+        if final_list[candidate] > winner_votes:
+            winner_votes = final_list[candidate]
+            winner_name = candidate
+        print(winner_name)
+
+    output = os.path.join("analysis", 'Electionresults.txt')
+    with open(output, "w") as new_analysis:
+        new_analysis.write("Election results")
+        new_analysis.write("\n")
+        new_analysis.write("--------------------")
+        new_analysis.write ("Total votes")
         
-        #Add Date
-        months.append(row[0])
+        
+    
 
-        profit_loss_change.append(profit_loss_change)
-        previous_month_profit_loss = current_month_profit_loss
+    # #Sort the list in ascending order
+    # sorted_list = sorted(voters_candidates)
 
-    #Sum average of the changes
-    sum_profit_loss = sum(profit_loss_change)
-    average_profit_loss = round(sum_profit_loss/(count_months - 1),2)
+    # #Arrange the sorted list by outcomes
+    # arange_list = sorted_list
 
-    #Highest and Lowest Changes in profit
-    highest_change = max(profit_loss_change)
-    lowest_change = min(profit_loss_change)
+    # #count votes per candidates in most common outcome order
+    # count_candidate = counter (arrange_list)
+    # votes_per_candidate.append(count_candidate.most_common())
 
-    #Value of highest and lowest changes in "profit/losses" over the entire period
-    highest_month_index = profit_loss_change.index(highest_change)
-    lowest_month_index = profit_loss_change.index(lowest_change)
+    # #Determine the percentage of votes per candidate in 3 digital points
+    # for item in votes_per_candidate:
 
-    #Assigning Best and worst month
-    best_month = months[highest_month_index]
-    worst_month = months[lowest_month_index]
+    #     first = format((item[0][1])*100/(sum(count_candidate.values())),'3f')
+    #     second = format((item[1][1])*100/(sum(count_candidate.values())),'3f')
+    #     third = format((item[2][1])*100/(sum(count_candidate.values())),'3f')
+    #     fourth = format((item[3][1])*100/(sum(count_candidate.values())),'3f')
 
-    #Print anylysis
-
-    print("Financial Analysis")
-    print("------------------------")
-    print(f"Total Months: {count_months}")
-    print(f"Total: ${net_profit_loss}")
-    print(f"Average Change: ${average_profit_loss}")
-    print(f"Greatest Increase in profits: {best_month} (${highest_change})")
-    print(f"Greatest Decrease in Losses: {worst_month} (${lowest_change})")
+    #  #Print analysis
+    # print("Election Results")
+    # print("---------------------------")
+    # print(f"Total Votes: {sum(count_candidate.values())}")
+    # print("---------------------------")
+    # print(f"{votes_per_candidate[0][0][0]}: {first}% {votes_per_candidate[0][0][1]})")
+    # print(f"{votes_per_candidate[0][1][0]}: {first}% {votes_per_candidate[0][1][1]})")
+    # print(f"{votes_per_candidate[0][2][0]}: {first}% {votes_per_candidate[0][2][1]})")
+    # print(f"{votes_per_candidate[0][3][0]}: {first}% {votes_per_candidate[0][3][1]})")
+    # print("---------------------------")
+    # print(f"Winner: {votes_per_candidate[0][0][0]}")
+    # print("---------------------------")
